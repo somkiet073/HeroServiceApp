8de7699d964d802f51e2207c51dce72bbe52460d
somkiet073 <somkiet073@users.noreply.github.com>
2020-10-11 16:20:07 +0700

    add Day2 Lock screen

--- lib/routers.dart
+++ lib/routers.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:heroService/screens/dashboard/dashboard_screen.dart';
+import 'package:heroService/screens/lockscreen/lock_screen.dart';
+import 'package:heroService/screens/welcome/welcome_screen.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
+  "/lockscreen": (BuildContext context) => LockScreen(),
+  "/welcome": (BuildContext context) => WelcomeScreen(),
   "/dashboard": (BuildContext context) => DashboardScreen(),
 };
--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:heroService/screens/bottomnav/booking_screen.dart';
 import 'package:heroService/screens/bottomnav/home_screen.dart';
+import 'package:heroService/screens/bottomnav/market_screen.dart';
+import 'package:heroService/screens/bottomnav/setting_screen.dart';
 
 class DashboardScreen extends StatefulWidget {
   DashboardScreen({Key key}) : super(key: key);
@@ -9,13 +12,53 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  // สร้างตัวแปร list เก็บรายการ tab
+  int _currentIndex = 0;
+  String _title = "บริการ";
+
+  final List<Widget> _childen = [
+    HomeScreen(),
+    MarketScreen(),
+    BookingScreen(),
+    SettingScreen()
+  ];
+
+  // สร้าง func ที่ใช้ในการเปลี่ยนหน้า
+  void onTabpped(int index) {
+    setState(() {
+      _currentIndex = index;
+
+      // เปลี่ยน title
+      switch (index) {
+        case 0:
+          _title = 'บริการ';
+
+          break;
+        case 1:
+          _title = 'ตลาด';
+
+          break;
+        case 2:
+          _title = 'รายการจอง';
+
+          break;
+        case 3:
+          _title = 'อื่นๆ';
+
+          break;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hero Service"),
+        title: Center(child: Text('$_title')),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: onTabpped,
         type: BottomNavigationBarType.fixed,
         // backgroundColor: Colors.teal,
         items: [
@@ -69,7 +112,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
           ),
         ],
       ),
-      body: HomeScreen(),
+      body: _childen[_currentIndex],
     );
   }
 }
